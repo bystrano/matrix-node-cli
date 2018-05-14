33d1d2368d86b038b7c7cc901982188edc7b7b01
Michel Bystranowski <bystrano@gmx.ch>
2018-05-14 14:34:04 +0200

    parsing the CLI's arguments

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 #!/usr/bin/env node
 
-console.log("hello world");
+
+/**
+ * Parsing the CLI arguments
+ */
+var program = require('commander');
+
+program
+    .version('0.0.1')
+    .option('-u --user', 'Specify the matrix user')
+    .option('-p --password', 'Specify the matrix password');
+
+program
+    .command('send <room> <message>')
+    .description('Send a message to a room')
+    .action(sendCommand);
+
+// If no known command, output help
+program
+    .command('*')
+    .action(function () {
+        program.outputHelp();
+    });
+
+program.parse(process.argv);
+
+
+/**
+ * Commands
+ */
+function sendCommand(room, msg) {
+
+    console.log('send ' + message + ' to ' + room);
+}

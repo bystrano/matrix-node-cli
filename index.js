507c8060d31fab266d4f9e7a78be08bd6417a268
Michel Bystranowski <bystrano@gmx.ch>
2018-05-15 16:22:03 +0200

    ask for the missing options interactively

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-
 /**
- * Parsing the CLI arguments
+ * Parse the CLI arguments and dispatch the commands.
  */
 var program = require('commander');
 
 program
     .version('0.0.1')
-    .option('-u --user', 'Specify the matrix user')
-    .option('-p --password', 'Specify the matrix password');
+    .option('-u --user <username>', 'Specify the matrix user')
+    .option('-p --password <password>', 'Specify the matrix password')
+    .option('-s --server <server>', 'Specify the matrix server');
 
 program
     .command('send <room> <message>')
@@ -27,9 +27,49 @@ program.parse(process.argv);
 
 
 /**
+ * Get the options, asking for input if necessary.
+ */
+var options = {};
+
+function getOptions() {
+    var co = require('co'),
+        prompt = require('co-prompt'),
+        options = {
+            user: program.user,
+            password: program.password,
+            server: program.server
+        };
+
+    return co(function* () {
+        if (options.user === undefined) {
+            options.user = yield prompt('username: ');
+        }
+
+        if (options.password === undefined) {
+            options.password = yield prompt.password('password: ');
+        }
+
+        if (options.server === undefined) {
+            options.server = yield prompt('server: ');
+        }
+
+        process.stdin.pause();
+
+        return options;
+    });
+}
+
+
+/**
  * Commands
  */
 function sendCommand(room, msg) {
 
-    console.log('send ' + message + ' to ' + room);
+    getOptions().then(function (options) {
+
+        console.log(options);
+        console.log('send ' + msg + ' to ' + room);
+
+        return options;
+    });
 }
